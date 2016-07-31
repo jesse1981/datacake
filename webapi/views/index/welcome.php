ca29ab39da0015fee7fa3affd42cb64ca69236e1
Pedro Fernández <pafmax@gmail.com>
2016-07-31 11:36:21 +1000

    added contend form sample

--- webapi/views/index/welcome.php
+++ webapi/views/index/welcome.php
@@ -12,57 +12,57 @@
   </head>
 
   <body>
-    <div id="floating-panel">
-      <button onclick="toggleHeatmap()">Toggle Heatmap</button>
-      <button onclick="changeGradient()">Change gradient</button>
-      <button onclick="changeRadius()">Change radius</button>
-      <button onclick="changeOpacity()">Change opacity</button>
+    <div id="top_panel">
+      <button id="top_button" onclick="toggleHeatmap()">Toggle Heatmap</button>
+      <button id="top_button" onclick="changeGradient()">Change gradient</button>
+      <button id="top_button" onclick="changeRadius()">Change radius</button>
+      <button id="top_button" onclick="changeOpacity()">Change opacity</button>
     </div>
     <div id="map"></div>
-	<div id="form">
-		<form>
-			<fieldset>
-				<legend>Crime</legend>
-				<label for="crime">Show</label><input type="checkbox" id="crime" class="enable-field" />
-				<div class="map-filter crime">
-					<label for="timeofday">Time of Day</label>
-					<select id="timeofday">
-						<option value="">Any</option>
-						<option value="Day">Day</option>
-						<option value="Night">Night</option>
-					</select>
-					<label for="dayofweek">Day of Week</label>
-					<select id="dayofweek">
-						<option value="">Any</option>
-						<option value="Weekday">Weekday</option>
-						<option value="Weekend">Weekend</option>
-					</select>
-				</div>
-			</fieldset>
-			<fieldset>
-				<legend>Infrastructure</legend>
-				<label for="infrastructre">Show</label><input type="checkbox" id="infrastructre" class="enable-field"/>
-				<div class="map-filter infrastructre">
-				
-				</div>
-			</fieldset>
-			<fieldset>
-				<legend>Businesses</legend>
-				<label for="businesses">Show</label><input type="checkbox" id="businesses" class="enable-field"/>
-				<div class="map-filter businesses">
-				
-				</div>
-			</fieldset>
-			<fieldset>
-				<legend>Weather</legend>
-				<label for="weather">Show</label><input type="checkbox" id="weather" class="enable-field" />
-				<div class="map-filter weather">
-				
-				</div>
-			</fieldset>
-		</form>
-	</div>
-    <script>	
+  	<div id="bottom_form">
+  		<form>
+  			<fieldset>
+  				<legend id="legend">Crime</legend>
+  				<label for="crime">Show</label><input type="checkbox" id="crime" class="enable-field" />
+  				<div class="map-filter crime">
+  					<label for="timeofday">Time of Day</label>
+  					<select id="timeofday">
+  						<option value="">Any</option>
+  						<option value="Day">Day</option>
+  						<option value="Night">Night</option>
+  					</select>
+  					<label for="dayofweek">Day of Week</label>
+  					<select id="dayofweek">
+  						<option value="">Any</option>
+  						<option value="Weekday">Weekday</option>
+  						<option value="Weekend">Weekend</option>
+  					</select>
+  				</div>
+  			</fieldset>
+  			<fieldset>
+  				<legend>Infrastructure</legend>
+  				<label for="infrastructre">Show</label><input type="checkbox" id="infrastructre" class="enable-field"/>
+  				<div class="map-filter infrastructre">
+  				
+  				</div>
+  			</fieldset>
+  			<fieldset>
+  				<legend>Businesses</legend>
+  				<label for="businesses">Show</label><input type="checkbox" id="businesses" class="enable-field"/>
+  				<div class="map-filter businesses">
+  				
+  				</div>
+  			</fieldset>
+  			<fieldset>
+  				<legend>Weather</legend>
+  				<label for="weather">Show</label><input type="checkbox" id="weather" class="enable-field" />
+  				<div class="map-filter weather">
+  				
+  				</div>
+  			</fieldset>
+  		</form>
+  	</div>
+      <script>	
 	$('.enable-field').click(function() {
 		if ($(this).prop("checked")) {
 			var id = $(this).attr('id');
